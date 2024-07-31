0f442d890ef7970f3cd899c72fbbc7b6f87c6758
Achamanp <144558579+Achamanp@users.noreply.github.com>
2024-08-01 02:12:19 +0530

    e_commercce

--- src/main/java/com/E_CommerceApplication/config/SecurityConfig.java
+++ src/main/java/com/E_CommerceApplication/config/SecurityConfig.java
@@ -7,6 +7,7 @@ import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
@@ -14,11 +15,14 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
 import com.E_CommerceApplication.impl.UserServiceImpl;
 import com.E_CommerceApplication.jwt.JwtFilter;
 @Configuration
 @EnableWebSecurity
+@EnableWebMvc
+@EnableMethodSecurity
 public class SecurityConfig {
 
     @Autowired
@@ -34,6 +38,7 @@ public class SecurityConfig {
         return httpSecurity.csrf(csrf -> csrf.disable())
                 .authorizeHttpRequests(auth -> auth
                         .requestMatchers("/login","api/v1/users/role/{id}","/api/v1/users/hello").permitAll()
+                        .requestMatchers("/swagger-ui/index.html").permitAll()
                         .anyRequest().authenticated())
                 .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
                 .authenticationProvider(authenticationProvider())
